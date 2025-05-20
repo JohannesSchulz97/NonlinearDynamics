451eca2971a3f8990dd73e595e22a2a756e29765
Johannes Schulz <schulz.johannes97@gmail.com>
2025-05-21 05:10:02 +0530

    maps2 complete pretty much

--- .ipynb_checkpoints/helper_functions-checkpoint.py
+++ .ipynb_checkpoints/helper_functions-checkpoint.py
@@ -1,13 +1,16 @@
 import numpy as np
 
-def logistic_map(x0, r, n):
+def logistic_iterator(x, r):
+    return r * x * (1-x)
+    
+def iterate_map(x0, r, n, iterator=logistic_iterator):
     x = x0
     xs = [x0]
     while n>0:
-        x = logistic_iterator(x,r)
+        x = iterator(x,r)
         xs.append(x)
         n -= 1
     return np.array(xs)
 
-def logistic_iterator(x, r):
-    return r * x * (1-x)
+def logistic_map(x0, r, n): 
+    return iterate_map(x0, r, n)
